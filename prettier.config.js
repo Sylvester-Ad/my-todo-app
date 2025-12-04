551e4a4ca220bad9e2be15ec95be617774933df2
Sylvester-Ad <sylvesteradade895@gmail.com>
2025-12-04 18:09:21 +0000

    First commit

---
+++ prettier.config.js
@@ -0,0 +1,7 @@
+export const semi = true;
+export const trailingComma = 'es5';
+export const singleQuote = false;
+export const printWidth = 100;
+export const tabWidth = 4;
+export const useTabs = false;
+export const arrowParens = 'always';
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+
+function App() {
+    return (
+        <div>
+            <h1>Todo App</h1>
+            <p>Welcome to your first TypeScript + React App</p>
+        </div>
+    );
+}
+
+export default App;
